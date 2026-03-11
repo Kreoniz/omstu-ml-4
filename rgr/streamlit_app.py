bc7b0d9585185cfb04b5b31bb19232bbd459719b
Kreoniz <kreoniz@mail.ru>
2026-03-11 10:41:02 +0600

    Add streamlit for rgr

---
+++ rgr/streamlit_app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(page_title="ML Dashboard", layout="wide")
+
+st.title("Welcome to My ML Dashboard")
+st.write("Use the sidebar to navigate between pages.")
